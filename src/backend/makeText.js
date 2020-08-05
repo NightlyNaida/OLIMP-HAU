24d7c2f2ceb70d2d0e8d8f373bef8c53f22866a1
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-08-05 08:58:57 +0300

    Модуль генерации текста сделан

--- src/backend/makeText.js
+++ src/backend/makeText.js
@@ -1,5 +1,18 @@
 let text2png = require('text2png');
 let fs = require('fs');
 
+let expFunc = async function (coefficents , callback){
+    if (coefficents instanceof Array){
+        let progressOfWork = [];
+        for (let i in coefficents){
+            let file = text2png(coefficents[i],{color: 'black', font: '75px Intro-Black', localFontPath:'./Intro-Black.otf', localFontName:'Intro-Black'});
+            fs.writeFile(`./coefficents/coeff${i}.png`,file,(err) => {
+                if (err) throw new Error(err);
+                progressOfWork.push('0');
+                if (progressOfWork.length == coefficents.length) callback();
+            });
+        }
+    }
+}
 
-fs.writeFileSync('coeff.png',text2png('3.53',{color: 'black', font: '75px Intro-Black', localFontPath:'./Intro-Black.otf', localFontName:'Intro-Black'}));
+expFunc(['2.57','2.12','1.10'],function(){console.log('OK')});
