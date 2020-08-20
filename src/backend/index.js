89b6a02a70e669901bfc4ec38923979943c72a56
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-08-20 08:59:43 +0300

    dfsdf
    sdf

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,9 @@
 let parsMatchPage = require('./parsMatchPage.js');
 new Promise (function(resolve,reject){
-    let matchData = parsMatchPage('https://www.olimp.bet/line/1/5927518/59104041',(err,data) => {if (err) new Error(err); else resolve(data)}); 
-}).then((matchData) => {console.log(matchData);})
+    //получаем данные с сайта
+    parsMatchPage('https://www.olimp.bet/live/1/6537877/59897726',(err,data) => {if (err) new Error(err); else resolve(data)}); 
+}).then((matchData) => {
+    
+})
 
 
