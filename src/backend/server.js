bcb39823c88d7abdfb35f7e32b259cbec476c849
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-08-26 23:19:27 +0300

    Начинаю писать сервер

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+let http = require('http');
+let fs = require('fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let server = http.createServer((req,res) => {
+    switch(req.url){
+      case'/': giveIndexPage(res); break;
+      default : notFound(res);
+    }
+  }
+).listen(3000, () => console.log('Server strat listening port 3000'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+function giveIndexPage(res){
+  let page = fs.readFileSync('../../build/index.html','utf-8');
+  res.writeHead(200,{'Content-Type' : 'text/html'});
+  res.end(page);
+}
+
+function notFound(res){
+  res.writeHead(404,{'Content-Type': 'text/plain'});
+  res.end('404 Not Found');
+}
