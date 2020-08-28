6a5f2e715859d738d4826f77a0122d44c3fae734
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-08-28 18:16:57 +0300

    asd

--- src/backend/icons.js
+++ src/backend/icons.js
@@ -2,23 +2,23 @@ let fs = require('fs'); //модуль принимает в себя назва
 
 let expFunc = async function(teamArr){
   let promiseArr = [];
-  for(let i = 0; i < 2; i++){
-    promiseArr.push(checkTeam(teamArr[i]));
-  }
+  promiseArr.push(checkTeam('firstTeam',teamArr[0]),checkTeam('firstTeam',teamArr[1]));
   return Promise.all(promiseArr);
 }
 
-let checkTeam = async function(team){
-  console.log('Opening JSON...');
+let checkTeam = async function(nameField,team){
+  console.log('Opening JSON...'); //открываем JSON, где хранятся настройки и путь до картинок
   let iconsObj = await new Promise((resolve,reject) => {
     fs.readFile('./iconsPath.json', 'utf-8', (err, fd) => {
       if (err) reject(err);
       resolve(JSON.parse(fd));
     });
   })
-  if (iconsObj.hasOwnProperty(team)) return true;
-  else return false;
-  ;
+  let objForReturn = {};
+  if (iconsObj.hasOwnProperty(team)) objForReturn[nameField] = true; //проверяем наличие логотипа
+  else objForReturn[nameField] = false; 
+  
+  return objForReturn
 }
 
 module.exports = expFunc;
--- src/backend/index.js
+++ src/backend/index.js
@@ -3,9 +3,7 @@ let icons = require('./icons.js');
 
 let matchData;
 parsMatchPage('https://www.olimp.bet/line/1/2430428/59915483').then(data => {matchData = data; console.log(matchData); return icons([matchData.firstTeam,matchData.secondTeam])})
-                                                              .then(checkResult => {if(checkResult.includes(false))
-                                                                    askImages(checkResult.map( (elem, index) => {if (elem) return index} ))
-                                                                })
+                                                              .then(checkResult => {console.log(checkResult)}); 
 
 
 
