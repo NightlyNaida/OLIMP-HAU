4f8b5b05befc1fd52b3e2f47dc72f4095958629b
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-09-06 22:21:41 +0300

    фв
    фыв

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,12 +2,23 @@ let http = require('http');
 let fs = require('fs');
 
 let server = http.createServer((req,res) => {
-    switch(req.url){
-      case'/': giveIndexPage(res); break;
-      default : notFound(res);
+      switch(req.method){
+        case 'POST': handlePOST(req,res); break;
+        case 'GET': handleGET(req,res); break;
+      }
     }
-  }
 ).listen(3000, () => console.log('Server strat listening port 3000'));
+
+function handleGET(req,res){
+  switch(req.url){
+    case'/': giveIndexPage(res); break;
+    default : give404Page(res);
+  }
+}
+
+function handlePOST(req,res){
+  
+}
 
 
 function giveIndexPage(res){
@@ -16,7 +27,7 @@ function giveIndexPage(res){
   res.end(page);
 }
 
-function notFound(res){
+function give404Page(res){
   res.writeHead(404,{'Content-Type': 'text/plain'});
   res.end('404 Not Found');
 }
